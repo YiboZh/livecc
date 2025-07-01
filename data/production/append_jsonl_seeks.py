0886446d0cb7297f211d5a49659c28abb96cf8ab
Yibo Zhao <yibozhao83@gmail.com>
2025-07-01 11:25:24 -0700

    update append script

--- data/production/append_jsonl_seeks.py
+++ data/production/append_jsonl_seeks.py
@@ -1,14 +1,15 @@
-import os, torch, json, tqdm
+import os, json
 
-root = 'llava_video_qwen_jsonl'
-files = os.listdir(root)
+path = os.path.join('/home/qua/Data/reaction_data/output_conversation_rewritten.jsonl')
 
-for file in tqdm.tqdm(files):
-    path = os.path.join(root, file)
-    if not path.endswith('.jsonl'):
-        continue
-    lines = open(path).readlines()
-    with open(path, 'a') as f:
-        seeks = [0] + torch.tensor([len(l) for l in lines]).cumsum(dim=-1)[:-1].tolist()
-        f.write(json.dumps(seeks))
+seeks = [0]
+with open(path, 'rb') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        seeks.append(f.tell())
+seeks = seeks[:-1]  # 去掉EOF位置
 
+with open(path, 'a') as f:
+    f.write(json.dumps(seeks))
